8d61f56a9c0b54f856aead3988563d5f73811f18
matthewgizzo <matthewjgizzo@gmail.com>
2021-03-05 22:21:28 -0600

    Finished adding a simple hello world controller

---
+++ PersonalWebsite/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public string Index()
+        {
+            return "This is my <b>default</b> action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int ID = 1)
+        {
+            return HttpUtility.HtmlEncode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
