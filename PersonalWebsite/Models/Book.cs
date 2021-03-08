b042c7b2199f91588238b5ef944ef9893cf0f833
matthewgizzo <matthewjgizzo@gmail.com>
2021-03-08 13:26:55 -0600

    Created the book model along with adding a db context for it to utilize EF Core

---
+++ PersonalWebsite/Models/Book.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PersonalWebsite.Models
+{
+    public class Book
+    {
+        [Key]
+        [Column("Id", TypeName = "bit")]
+        public int ID { get; set; }
+        
+        [Column("Title", TypeName = "varchar(100)")]
+        public string Title { get; set; }
+
+        [Column("Author", TypeName = "varchar(50)")]
+        public string Author { get; set; }
+
+        [Column("ReleaseDate", TypeName = "datetime")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Column("Genre", TypeName = "varchar(50)")]
+        public string Genre { get; set; }
+
+        [Column("Series", TypeName = "varchar(100)")]
+        public string Series { get; set; }
+
+        [Column("WikipediaLink", TypeName = "varchar(500)")]
+        public string WikipediaLink { get; set; }
+    }
+
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
